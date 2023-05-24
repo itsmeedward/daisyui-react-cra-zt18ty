b22fb2b601b00f22f9d47a28b5c085c0226ab7ea
itsmeedward <86776978+itsmeedward@users.noreply.github.com>
2023-05-24 17:30:42 +0200

    1 file modified, 1 added

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowseRouter as Router, link } from 'react-dom';
 import Fifth from './heroes/fifth';
 import First from './heroes/first';
+import First101 from './heroes/first1.1';
 import Fourth from './heroes/fourth';
 import Second from './heroes/second';
 import Seventh from './heroes/seventh';
@@ -9,7 +10,6 @@ import Sixth from './heroes/sixth';
 import Third from './heroes/third';
 
 export default function Main() {
-  
   const glass = {
     backgroundColor: 'rgba(255, 255, 255, 0.3)',
     backdropFilter: 'blur(10px)',
@@ -18,17 +18,15 @@ export default function Main() {
 
   return (
     <>
-          
+      <First101 />
       <First />
       <Second />
       <Third />
       <Fourth />
-      
+
       <Fifth />
       <Seventh />
       <Sixth />
-
-   
     </>
   );
 }
