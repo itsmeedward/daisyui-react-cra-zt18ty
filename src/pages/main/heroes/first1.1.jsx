ab3906c79407a5548cdd65307218ad4bb95a7248
itsmeedward <86776978+itsmeedward@users.noreply.github.com>
2023-05-30 02:26:10 +0200

    4 files modified

--- src/pages/main/heroes/first1.1.jsx
+++ src/pages/main/heroes/first1.1.jsx
@@ -46,7 +46,7 @@ const First101 = () => {
         </div>
         <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
           <div className="relative max-w-2xl sm:mx-auto sm:max-w-xl md:max-w-2xl sm:text-center">
-            <h2 className="mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
+            <h2 className="mb-6 font-sans text-3xl font-bold font-medium text-7xl md:text-10xl xl:text-11xl leading-tight  text-white sm:text-4xl sm:leading-none">
               Transforming the Digital Landscape 
               <br className="hidden md:block" />
               with{' '}
--- src/pages/main/test.jsx
+++ src/pages/main/test.jsx
@@ -6,66 +6,54 @@ const Test = () => {
     <section
     data-section-id={1}
     data-share=""
-    data-category="features"
-    data-component-id="4d2f33a1_04_awz"
-    className="py-32 bg-white overflow-hidden"
+    data-category="content"
+    data-component-id="fdca0d23_05_awz"
+    className="relative py-24 2xl:pt-44 2xl:pb-52 bg-white overflow-hidden"
   >
     <div className="container px-4 mx-auto">
-      <div className="flex flex-wrap items-center -m-8">
-        <div className="w-full md:w-1/2 p-8">
+      <div className="mb-20 lg:mb-0 w-full lg:w-1/2">
+        <div className="pb-14 xl:pb-20 mb-14 xl:mb-28 border-b border-gray-100">
           <h2
-            className="mb-9 text-6xl md:text-8xl xl:text-10xl font-bold font-heading tracking-px-n leading-none"
-            data-config-id="auto-txt-1-4"
+            className="font-heading font-medium text-7xl md:text-10xl xl:text-11xl leading-tight"
+            data-config-id="auto-txt-1-5"
           >
-            Get down to your business
+            Your best website starts here
           </h2>
-          <p
-            className="mb-10 text-lg text-gray-900 font-medium leading-relaxed md:max-w-md"
-            data-config-id="auto-txt-2-4"
-          >
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-            sint. Velit officia consequat duis enim velit mollit. Exercitation
-            veniam consequat.
-          </p>
-          <div className="mb-11 md:inline-block rounded-xl shadow-4xl">
-            <button
-              className="py-4 px-6 w-full text-white font-semibold border border-indigo-700 rounded-xl focus:ring focus:ring-indigo-300 bg-indigo-600 hover:bg-indigo-700 transition ease-in-out duration-200"
-              type="button"
-              data-config-id="auto-txt-3-4"
-            >
-              Learn How to Save More
-            </button>
-          </div>
-          <div className="flex flex-wrap -m-2">
-            <div className="w-auto p-2">
-              <img
-                src="https://shuffle.dev/flaro-assets/images/features/feature2.png"
-                alt=""
-                data-config-id="auto-img-1-4"
-              />
-            </div>
-            <div className="flex-1 p-2">
-              <p
-                className="text-gray-600 font-medium md:max-w-sm"
-                data-config-id="auto-txt-4-4"
-              >
-                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-                amet sint. Velit officia consequat duis enim velit mollit.
-              </p>
-            </div>
-          </div>
         </div>
-        <div className="w-full md:w-1/2 p-8">
-          <img
-            className="transform hover:-translate-x-16 transition ease-in-out duration-1000"
-            src="https://wallpapercave.com/wp/wp8593341.jpg"
-            alt=""
-            data-config-id="auto-img-2-4"
-          />
-        </div>
+        <span
+          className="block mb-12 font-medium uppercase tracking-widest text-xs text-gray-300 leading-4"
+          data-config-id="auto-txt-2-5"
+        >
+          Discover special tools
+        </span>
+        <h3
+          className="mb-14 font-heading font-medium text-3xl xl:text-5xl leading-snug max-w-2xl"
+          data-config-id="auto-txt-3-5"
+        >
+          A spatial system is a set of rules for how you measure and size your UI
+          elements.
+        </h3>
+        <p
+          className="mb-14 xl:mb-36 text-lg leading-10 text-darkBlueGray-400 max-w-md"
+          data-config-id="auto-txt-4-5"
+        >
+          Proin nec nibh vel odio dapibus molestie eu id ipsum. Fusce sodales
+          vitae lorem vitae tempus. Etiam nisi ligula, placerat at congue et,
+          vehicula quis ex.
+        </p>
+        
+      </div>
+      <div className="relative lg:absolute top-0 right-0 lg:mt-12 w-full lg:w-1/2">
+        <img
+          className="ml-auto"
+          src="https://shuffle.dev/uinel-assets/images/content/widget5.png"
+          alt=""
+          data-config-id="auto-img-2-5"
+        />
       </div>
     </div>
   </section>
+  
   
   
   );
